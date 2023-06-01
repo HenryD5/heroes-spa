adeaf20f9a43e246c419fe4c7836f815d519d4c2
Henry David Yamunaque Villegas <david_yamunaque_5@outlook.com>
2023-06-01 00:02:41 -0500

    feat: pages, hooks and components

---
+++ src/heroes/helpers/getHeroesByPublisher.js
@@ -0,0 +1,9 @@
+import { heroes } from "../data/heroes";
+
+export const getHeroesByPublisher = (publisher) => {
+  const validPubliser = ["DC Comics", "Marvel Comics"];
+  if (!validPubliser.includes(publisher)) {
+    throw new Error(`${publisher} is not a valid publisher`);
+  }
+  return heroes.filter((heroe) => heroe.publisher === publisher);
+};
